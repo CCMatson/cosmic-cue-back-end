0b431f24f41ebab4d595eb7d9b248b62db707323
Kendra Lacey <101334078+kendra-lacey@users.noreply.github.com>
2023-02-04 22:07:27 -0500

    add requirements to blog schema

--- controllers/signs.js
+++ controllers/signs.js
@@ -2,13 +2,7 @@ import { Sign } from "../models/sign";
 
 
 const create = async (req,res) => {
-  try {
-    const sign = await Sign.create(req.body)
-    res.status(201).json(sign)
-  } catch (error) {
-    console.log(error) 
-    res.status(500).json(error)
-  }
+
 }
 
 
