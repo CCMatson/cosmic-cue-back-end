030cb6420e5933f46e7ee894a349145893ad5b91
Kendra Lacey <101334078+kendra-lacey@users.noreply.github.com>
2023-02-04 21:46:02 -0500

    add requirements to fortune schema

--- models/fortune.js
+++ models/fortune.js
@@ -3,11 +3,18 @@ import mongoose from 'mongoose'
 const Schema = mongoose.Schema
 
 const fortuneSchema = new Schema({
-  message: String,
-  luckyNumber: Number,
+  message: {
+    type: String,
+    required: true,
+  },
+  luckyNumber: {
+    type: Number,
+    required: true,
+  },
   owner: {type: Schema.Types.ObjectId, ref: "Profile"},
   category: {
     type: String,
+    required: true,
     enum: ['Love','Travel','Work','Food']
   }
 },{
