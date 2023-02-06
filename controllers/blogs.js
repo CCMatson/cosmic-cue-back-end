f1841c04394c3a419a1aaade82b23e88c9147f35
Kendra Lacey <101334078+kendra-lacey@users.noreply.github.com>
2023-02-06 18:26:43 -0500

    code and export update controller

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -39,9 +39,23 @@ const show = async (req, res) => {
   }
 }
 
+const update = async (req, res) => {
+  try {
+    const blog = await Blog.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    ).populate('owner')
+    res.status(200).json(blog)
+  }  catch (error) {
+    res.status(500).json(error)
+  }
+}
+
 export {
   create,
   index,
-  show
+  show,
+  update,
 }
 
