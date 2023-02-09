c68f7fc7350a9ef4636b3fe0783ed41f24bff487
CCMatson <catherine.matson@gmail.com>
2023-02-09 10:17:09 -0800

    change fortunes route to public

--- routes/fortunes.js
+++ routes/fortunes.js
@@ -5,13 +5,13 @@ import { decodeUserFromToken, checkAuth } from '../middleware/auth.js'
 const router = Router()
 
 // ========== Public Routes ===========
+router.get('/', checkAuth, fortunesCtrl.index)
 
 
 
 // ========= Protected Routes ========= 
 router.use(decodeUserFromToken)
 router.post('/' , checkAuth, fortunesCtrl.create)
-router.get('/', checkAuth, fortunesCtrl.index)
 router.get('/:id', checkAuth, fortunesCtrl.show)
 router.put('/:id', checkAuth, fortunesCtrl.update)
 router.delete('/:id' , checkAuth, fortunesCtrl.delete)
